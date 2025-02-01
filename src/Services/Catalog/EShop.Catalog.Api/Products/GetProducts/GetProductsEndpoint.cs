a05b97d0eccfc597b885ea55c0afb916935d6dea
Alex Canario <alexcanario@gmail.com>
2025-02-01 10:33:16 +0000

    Add new endpoint for fetching products in EShop API
    Introduce a new endpoint and handler for fetching products in the EShop catalog API.
    - Create namespace `EShop.Catalog.Api.Products.GetProducts`.
    - Add `GetProductsResponse` record to encapsulate response.
    - Implement `GetProductsEndpoint` class for `/products` route.
    - Configure `GetProductsEndpoint` with metadata and response types.
    - Define `GetProductsQuery` record for pagination parameters.
    - Define `GetProductsResult` record for query results.
    - Implement `GetProductsHandler` to handle queries, log, fetch products, and return results.

---
+++ src/Services/Catalog/EShop.Catalog.Api/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace EShop.Catalog.Api.Products.GetProducts;
+
+//public sealed record GetProductsRequest(int PageSize = 1, int PageNumber = 10); 
+public sealed record GetProductsResponse(IEnumerable<Product> Products);
+
+public sealed class GetProductsEndpoint : ICarterModule
+{
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapGet("/products", async (ISender sender) =>
+		{
+			var products = await sender.Send(new GetProductsQuery());
+			var response = products.Adapt<GetProductsResponse>();
+			return Results.Ok(response);
+		})
+			.WithName("GetProducts")
+			.WithDescription("Get all products")
+			.WithSummary("Get all products")
+			.Produces<GetProductsResponse>(StatusCodes.Status200OK)
+			.ProducesProblem(StatusCodes.Status404NotFound)
+			.ProducesProblem(StatusCodes.Status400BadRequest);
+	}
+}
---
+++ src/Services/Catalog/EShop.Catalog.Api/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,19 @@
+﻿using Marten.Pagination;
+
+namespace EShop.Catalog.Api.Products.GetProducts;
+
+internal sealed record GetProductsQuery(int PageNumber = 1, int PageSize = 10) : IQuery<GetProductsResult>;
+internal sealed record GetProductsResult(IEnumerable<Product> Products);
+
+internal sealed class GetProductsHandler(IDocumentSession Session, ILogger<GetProductsHandler> Logger)
+	: IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+	public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+	{
+		Logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+
+		var products = await Session.Query<Product>().ToPagedListAsync(query.PageNumber, query.PageSize, cancellationToken);
+
+		return new(products);
+	}
+}
