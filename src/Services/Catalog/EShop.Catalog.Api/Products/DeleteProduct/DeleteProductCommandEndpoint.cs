7ccc72faa718a3938b60f6e89bc4c93ffc302e3f
Alex Canario <alexcanario@gmail.com>
2025-02-02 18:45:21 +0000

    feat: add feature to delete products in EShop catalog API
    Introduce new endpoint and handler for deleting products.
    - Add `DeleteProductCommandEndpoint` with DELETE route.
    - Define `DeleteProductResponse` for delete operation response.
    - Implement `DeleteProductCommandHandler` to handle delete command.
    - Define `DeleteProductCommand` and `DeleteProductResult` records.

---
+++ src/Services/Catalog/EShop.Catalog.Api/Products/DeleteProduct/DeleteProductCommandEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+namespace EShop.Catalog.Api.Products.DeleteProduct;
+
+//public sealed record DeleteProductRequest(Guid ProductId)
+public sealed record DeleteProductResponse(bool IsSuccess);
+
+public class DeleteProductCommandEndpoint : ICarterModule
+{
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapDelete("/products/{id}", async (Guid id, ISender sender) => 
+		{ 
+			var result = await sender.Send(new DeleteProductCommand(id));
+			var response = result.Adapt<DeleteProductResponse>();
+			return Results.Ok(new DeleteProductResponse(result.IsSuccess));
+
+		})
+			.WithName("DeleteProduct")
+			.WithDescription("Delete a product by id")
+			.WithSummary("Delete a product by id")
+			.Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+			.ProducesProblem(StatusCodes.Status404NotFound)
+			.ProducesProblem(StatusCodes.Status400BadRequest);
+	}	
+}
---
+++ src/Services/Catalog/EShop.Catalog.Api/Products/DeleteProduct/DeleteProductCommandHanlder.cs
@@ -0,0 +1,16 @@
+﻿namespace EShop.Catalog.Api.Products.DeleteProduct;
+
+public sealed record DeleteProductCommand(Guid ProductId) : ICommand<DeleteProductResult>;
+public sealed record DeleteProductResult(bool IsSuccess);
+
+internal class DeleteProductCommandHanlder(IDocumentSession Session, ILogger<DeleteProductCommandHanlder> Logger) 
+	: ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+	public async Task<DeleteProductResult> Handle(DeleteProductCommand Command, CancellationToken cancellationToken)
+	{
+		Logger.LogInformation("Deleting product at DeleteProductCommandHanlder.Handle with id {ProductId}", Command.ProductId);
+		Session.Delete<Product>(Command.ProductId);
+		await Session.SaveChangesAsync(cancellationToken);
+		return new (true);
+	}
+}
