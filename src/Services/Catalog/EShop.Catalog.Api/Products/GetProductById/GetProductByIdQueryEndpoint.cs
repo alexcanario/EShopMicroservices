4b3d48f6b56da6e0e9046abc51be51891f36c96d
Alex Canario <alexcanario@gmail.com>
2025-02-02 17:23:24 +0000

    Add GetProductById feature to EShop.Catalog.Api
    Introduce a new feature for retrieving product details by ID.
    - Create namespace `EShop.Catalog.Api.Products.GetProductById`.
    - Add `GetProductByIdResponse` record for response encapsulation.
    - Implement `GetProductByIdQueryEndpoint` with route `/products/{id}`.
    - Use `ISender` to handle `GetProductByIdQuery` and map to response.
    - Add metadata for route including name, description, and response types.
    - Introduce `GetProductByIdResult` record for query result encapsulation.
    - Add `GetProductByIdQuery` record implementing `IQuery<GetProductByIdResult>`.
    - Implement `GetProductByIdQueryHandler` to handle the query, log details, load product, and handle `ProductNotFoundException`.

---
+++ src/Services/Catalog/EShop.Catalog.Api/Products/GetProductById/GetProductByIdQueryEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace EShop.Catalog.Api.Products.GetProductById;
+
+internal sealed record GetProductByIdResponse(Product Product);
+//internal sealed record GetProductByIdRequest(Guid Id);
+public class GetProductByIdQueryEndpoint : ICarterModule
+{
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+		{
+			var result = await sender.Send(new GetProductByIdQuery(id));
+			var response = result.Adapt<GetProductByIdResponse>();
+			return Results.Ok(response);
+		})
+			.WithName("GetProductById")
+			.WithDescription("Get product by id")
+			.WithSummary("Get product by id")
+			.Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+			.ProducesProblem(StatusCodes.Status404NotFound)
+			.ProducesProblem(StatusCodes.Status400BadRequest);
+	}
+}
---
+++ src/Services/Catalog/EShop.Catalog.Api/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace EShop.Catalog.Api.Products.GetProductById;
+
+public sealed record GetProductByIdResult(Product Product);
+public sealed record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public class GetProductByIdQueryHandler(IDocumentSession Session, ILogger<GetProductByIdQueryHandler> Logger) 
+	: IRequestHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+	public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+	{
+		Logger.LogInformation("Querying product at GetProductByIdHandler.Handle called with {@Query}", query);
+
+		var product = await Session.LoadAsync<Product>(query.Id, cancellationToken);
+
+		return product is null 
+			? throw new ProductNotFoundException() 
+			: new GetProductByIdResult(product);
+	}
+}
