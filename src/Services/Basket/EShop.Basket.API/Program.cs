ec728e8f685b46f9294f3deaaead1ab28aa76c1b
Alex Canario <alexcanario@gmail.com>
2025-02-11 08:13:02 +0000

    Add EShop.Basket.API project with initial setup
    Introduce new "Basket" project and its API "EShop.Basket.API" to the solution file `EShop.sln`. Add `appsettings.Development.json` and `appsettings.json` for configuration. Include a `Dockerfile` for Docker image build process. Create `EShop.Basket.API.csproj` with project settings and package references. Add `Program.cs` to set up the web application builder and HTTP request pipeline. Add `launchSettings.json` for different running profiles.

---
+++ src/Services/Basket/EShop.Basket.API/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+#region Add services to the container.
+
+#endregion
+
+var app = builder.Build();
+
+#region Configure the HTTP request pipeline.
+
+#endregion
+
+app.Run();
