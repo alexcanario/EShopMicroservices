afa87ff93eae4f4cdebae9fdb3439191c49dd78c
Alex Canario <alexcanario@gmail.com>
2025-07-10 21:51:10 +0100

    Add feature management for integration event publishing
    Updated appsettings files to enable the PublishIntegrationEvents feature.
    Modified OrderCreatedEventHandler to conditionally publish integration events
    based on the feature flag, enhancing control over event publishing.

--- src/Services/Ordering/EShop.Ordering.App/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/EShop.Ordering.App/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,16 +1,19 @@
-﻿namespace EShop.Ordering.App.Orders.EventHandlers.Domain;
+﻿using Microsoft.FeatureManagement;
 
-public class OrderCreatedEventHandler(IPublishEndpoint publishEndpoint, ILogger<OrderCreatedEventHandler> logger) 
+namespace EShop.Ordering.App.Orders.EventHandlers.Domain;
+
+public class OrderCreatedEventHandler(IPublishEndpoint publishEndpoint, ILogger<OrderCreatedEventHandler> logger, IFeatureManager featureManager) 
 	: INotificationHandler<OrderCreatedEvent>
 {
     public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain event handled: {DomainEvent}", domainEvent.GetType().Name);
 
-        var orderCreatedIntegrationEvent = domainEvent.Order.ToOrderDto();
-
-        await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
-
-        logger.LogInformation("Integration event published: {IntegrationEvent}", orderCreatedIntegrationEvent.GetType().Name);
+        if(await featureManager.IsEnabledAsync("PublishIntegrationEvents", cancellationToken))
+        {
+			var orderCreatedIntegrationEvent = domainEvent.Order.ToOrderDto();
+			await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+			logger.LogInformation("Integration event published: {IntegrationEvent}", orderCreatedIntegrationEvent.GetType().Name);
+		}
 	}
 }
