645b1d2931763038101a33e5f643411665dcce22
Alex Canario <alexcanario@gmail.com>
2025-07-21 09:02:38 +0100

    Refactor models and update database connection settings
    - Initialize properties in BasketItemsCheckoutEvent for better defaults.
    - Add JsonConstructor attribute and change property accessors in ShoppingCart.
    - Change Id to immutable and initialize string properties in Product.
    - Add DiscountConnection string in appsettings.json for new database.
    - Include discount.db binary file in the project.
    - Update docker-compose to use the correct database connection string.

--- src/Services/Basket/EShop.Basket.API/Models/ShoppingCart.cs
+++ src/Services/Basket/EShop.Basket.API/Models/ShoppingCart.cs
@@ -1,11 +1,15 @@
-﻿namespace EShop.Basket.API.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace EShop.Basket.API.Models;
 
 public sealed class ShoppingCart
 {
-	public ShoppingCart() { }
+    [JsonConstructor]
+    public ShoppingCart() { }
+    
 	public ShoppingCart(string username) => Username = username;
 
-	public string Username { get; private set; } = string.Empty;
-	public IList<ShoppingCartItem> Items { get; private set; } = new List<ShoppingCartItem>();
+	public string Username { get; set; } = string.Empty;
+	public IList<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
 	public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
 }
